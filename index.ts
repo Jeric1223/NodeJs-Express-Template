e76b2f92ba7b3d11dada221898a71229ecff4cfa
JaeHyun <soehd0889@cntech.pro>
2023-04-06 13:58:29 +0900

    npm i dotenv

--- index.ts
+++ index.ts
@@ -1,12 +1,4 @@
-import express, { Express, Request, Response } from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
 
-const app: Express = express();
-const port = 3000;
-
-app.get('/', (req: Request, res: Response) => {
-  res.send('Typescript + Node.js + Express Server');
-});
-
-app.listen(port, () => {
-  console.log(`[server]: Server is running at <https://localhost>:${port}`);
-});
+import './src/server';
